1fe89c870d544a357e9535c1615dd852e1e20901
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-23 21:50:02 +0530

    Update Relationships and Foreign keys.sql

--- Relationships and Foreign keys.sql
+++ Relationships and Foreign keys.sql
@@ -1,1 +1,6 @@
+-- What are Relationships, Foreign keys and Normalization
+--Relationship is relating one table to another using primary and foreign keys
+--A Table id that is included in another table is foriegn key to that table.
+--Reducing redundancy and repeatation of data by using foriegn keys is Normalization.
+
 
