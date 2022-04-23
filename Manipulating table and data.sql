b299e719b4f38daab73c77c0bd6af793f8fd36c7
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-23 08:13:07 +0530

    Update Manipulating table and data.sql

--- Manipulating table and data.sql
+++ Manipulating table and data.sql
@@ -7,7 +7,7 @@ DROP DATABASE school_test	--It deletes the database school_test
 use school;  -- This command is to use which database to create table, under that.
 create table Students (
 id int primary key not null identity(1,1), -- In this id is column name, int is datatype used for that, idetity to start the id from 1 and increment its next value with increment of 1.
-lastname nvarchar(50) not null,
+lastname nvarchar(50) not null,     -- nvarchar datatype is to create variable that can include characters, numbers, and any letters.
 firstname nvarchar(50) not null,
 dateofbirth datetime not null,
 dateofjoining datetime 
