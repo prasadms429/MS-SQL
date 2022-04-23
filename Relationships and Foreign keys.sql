c270cf1f1143ab3421ecdd0928754b41c5afebf8
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-23 23:33:46 +0530

    Update Relationships and Foreign keys.sql

--- Relationships and Foreign keys.sql
+++ Relationships and Foreign keys.sql
@@ -3,4 +3,4 @@
 --A Table id that is included in another table is foriegn key to that table.
 --Reducing redundancy and repeatation of data by using foriegn keys is Normalization.
 
-
+-- Table which is not dependent on any other table then it consist atomicity property. If it includes any forieng key in it then it is not consist atomicity.
