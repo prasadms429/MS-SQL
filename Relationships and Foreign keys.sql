097f330de15adaeb58c02f4db20500e0e3d8f80a
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-24 17:01:38 +0530

    Update Relationships and Foreign keys.sql

--- Relationships and Foreign keys.sql
+++ Relationships and Foreign keys.sql
@@ -5,6 +5,7 @@
 
 -- Table which is not dependent on any other table then it consist atomicity property. If it includes any forieng key in it then it is not consist atomicity.
 
+
 --Running queries on related table ( Inner join )
 --Joins
 select
@@ -19,3 +20,22 @@ inner join Teacher t on t.id= e.TeacherId       -- We denote Teacher as 't' beca
 inner join Students s on s.id= e.StudentId      -- In this, 'Students' is a table in that it contains 'id' and that information is completly joined to 'StudentId' in 'Enrollment table'.
 inner join Courses c on c.CourseId= e.CourseId
 where t.Id=2                                    -- It displays above selected information for teacher id 2, in enrollment table.
+
+
+-- LEFT, RIGHT AND FULL JOIN
+-- Only slects records that have values that meet the condition.
+-- Records that do not meet the condition will not be returned in the result set.
+select * from Enrollments e
+inner join Teachers t on e.TeacherId=t.id
+
+-- Will select all values in the 'left table', whether or not they meet the
+-- condition. Be sure to structure the query correctly to bring back the records
+-- that you are interested in.
+select * from Enrollments e
+left join Teachers t on e.TeacherId=t.id
+
+-- Will select all values in the 'right table', whether or not they meet the
+-- condition. Be sure to structure the query correctly to bring back the records
+-- that you are interested in.
+select * from Enrollments e
+right join Teachers t on e.Teacherid=t.id
