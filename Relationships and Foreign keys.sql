0c11de26790584e1d32ed0e447d270946145f47a
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-24 20:07:06 +0530

    Update Relationships and Foreign keys.sql

--- Relationships and Foreign keys.sql
+++ Relationships and Foreign keys.sql
@@ -39,3 +39,18 @@ left join Teachers t on e.TeacherId=t.id
 -- that you are interested in.
 select * from Enrollments e
 right join Teachers t on e.Teacherid=t.id
+
+-- GROUPING DATA USING THE SQL GROUP BY CLAUSE
+ select TeacherId, CourseId from Enrollments            -- To select two columns and display it from Enrollments table.
+ select distinct TeacherId, CourseId from Enrollments   -- To select reduce repeated rows that contains same value in rows in a selected columns, we use this.
+ 
+ SELECT TOP (1000) 
+	   [TeacherId]
+      ,[StudentId],
+	  s.firstname+' '+s.lastname [Student Name],
+	  t.firstname+' '+t.lastname [Teacher Name]
+  FROM [school].[dbo].[Enrollments]
+  inner join Teachers t on t.id=TeacherId
+  inner join Students s on s.id=StudentId
+  where TeacherId=2
+  group by [TeacherId],[StudentId],s.firstname,s.lastname,t.firstname,t.lastname    -- Group by is used to do some mathematical operations.
