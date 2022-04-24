06e8239e67e5fa6dbc14f211ab448a96bb9cfb53
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-24 21:54:18 +0530

    Update Group and Aggregate Functions.sql

--- Group and Aggregate Functions.sql
+++ Group and Aggregate Functions.sql
@@ -12,3 +12,12 @@
   inner join Students s on s.id=StudentId
   where TeacherId=2
   group by [TeacherId],[StudentId],s.firstname,s.lastname,t.firstname,t.lastname    -- Group by is used to do some mathematical operations.
+  
+  
+  -- SQL AVERAGE AGGREGATE FUNCTION
+ use school;
+select CourseId,avg(Grade) from Enrollments
+--where CourseId=2 		-- If use this then it prints for specific id, otherwise it prints for all ids
+group by CourseId		-- By using group by CourseId it calculates average values by grouping common ids. For all different course ids.
+having avg(Grade)>=70		-- If we want to apply aggregate function for specific condition then we use 'having' keyword.
+order by avg(grade)		-- To print average values of grade in asending order we use this.
