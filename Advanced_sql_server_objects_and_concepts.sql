389b55b63768381ac7f796e292a6da44716d5b3c
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-04-25 17:17:27 +0530

    Update Advanced_sql_server_objects_and_concepts.sql

--- Advanced_sql_server_objects_and_concepts.sql
+++ Advanced_sql_server_objects_and_concepts.sql
@@ -31,3 +31,86 @@ where [Course Code]='CS202'                 -- We are using some condition to di
 order by [Course Title]                      -- We cannot use 'order by' in above query of create view, but we can use in this. We should use '[]' this, because Course Title contain space.
 
 exec sp_rename 'Student_Subject_Lectureres','Student Enrollements'   -- To rename the view file we use this. In this 'exec' means execute, 'sp_rename' means stored procedure rename.
+
+
+-- SCALAR VALUED FUNCTIONS
+
+use school;
+select max(grade) from Enrollments    -- In this we use built in function to do get maximum grade.
+
+select GETDATE()                      -- It gives specific year, month, date and time at this moment. In this we use built in function
+
+select Year(GETDATE())                -- It gives only year. In this we use built in function.
+
+-- This is user defined function.
+select dbo.getHighestGrade()
+
+select dbo.getLowestGrade()
+
+select dbo.getCourseTitleById(5)
+
+-- The description of user defined functions as follows
+-- For getHighestGrade
+USE [school]
+GO
+/****** Object:  UserDefinedFunction [dbo].[getHighestGrade]    Script Date: 25-04-2022 17:13:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+Create FUNCTION getHighestGrade 
+(
+	-- Add the parameters for the function here
+)
+RETURNS float
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @grade float
+
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @grade=max(grade) from Enrollments
+
+	-- Return the result of the function
+	RETURN @grade
+
+END
+
+
+-- To getCourseTileById
+USE [school]
+GO
+/****** Object:  UserDefinedFunction [dbo].[getCourseTitleById]    Script Date: 25-04-2022 17:16:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION getCourseTitleById 
+(
+	-- Add the parameters for the function here
+	@courseid int
+)
+RETURNS nvarchar(50)
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @name nvarchar(50)
+
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @name=CourseName from Courses where CourseId=@courseid
+
+	-- Return the result of the function
+	RETURN @name
+
+END
+
